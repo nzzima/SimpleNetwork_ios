aef29e46a2ae586464ae59afca8e400aa356840c
nzzima <nikita-krylov-2012@mail.ru>
2024-08-30 15:26:24 +0300

    Added codable structure and parsing unic parts of first console output

--- SimpleNetwork/ViewController.swift
+++ SimpleNetwork/ViewController.swift
@@ -14,10 +14,20 @@ class ViewController: UIViewController {
         var request = URLRequest(url: URL(string: "https://v2.jokeapi.dev/joke/Any")!)
         request.httpMethod = "GET"
         request.allHTTPHeaderFields = ["AuthToken": "null"]
+    
+        /**
+         Здесь мы проводим две проверки:
+
+         Проверка того, что data не пустая;
+         Проверка того, что data можно декодировать по структуре Joke, которую мы создали на предыдущем шаге.
+
+         Если обе проверки пройдены, то мы запускаем следующий код: print(joke.setup)
+         */
+        
         let task = URLSession.shared.dataTask(with: request) { data, response, error in
-        if let data = data, let joke = try? JSONDecoder().decode(Joke.self, from: data) {
-            print(joke.category!)
-        }
+            if let data = data, let joke = try? JSONDecoder().decode(Joke.self, from: data) {
+                print(joke.setup!)
+            }
         }
         task.resume()
     }
